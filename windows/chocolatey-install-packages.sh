c751d1c5e80e62ef3eac06be9f01b1fea865138d
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-05-21 13:44:10 -0500

    add windows setup stuffs

---
+++ windows/chocolatey-install-packages.sh
@@ -0,0 +1,42 @@
+﻿
+#System
+choco install amd-ryzen-chipset
+choco install wsl2 --params "/Version:2 /Retry:true"
+choco install eartrumpet
+choco install bulk-crap-uninstaller
+choco install 7zip
+
+#DEV Tools
+choco install ultraedit
+choco install activeperl
+choco install python3
+choco install racket
+
+#C++ Tooling
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+choco install ninja
+choco install llvm
+
+#Java
+choco install AdoptOpenJDK --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome /INSTALLDIR=C:\Program Files\AdoptOpenJDK\ /quiet"
+
+#Applications
+choco install foxitreader --ia '/MERGETASKS="!desktopicon /COMPONENTS=*pdfviewer,*ffse,*installprint,*ffaddin,*ffspellcheck,!connectedpdf"'
+choco install microsoft-office-deployment --params '/64bit  /Product:O365HomePremRetail  /Exclude=Publisher,Lync,Groove,Access'
+# Maybe????  O365PersonalRetail    https://www.tenforums.com/tutorials/123233-custom-install-change-microsoft-office-office-deployment-tool.html
+choco install authy-desktop
+choco install speedtest
+
+#Content Creation
+choco install ffmpeg-full
+choco install sox.portable
+#choco install audacity
+choco install openshot
+choco install inkscape
+
+#OBS
+choco install obs-studio
+choco install obs-virtualcam
+
+#Games
+choco install retroarch
---
+++ windows/system-setup.ps1
@@ -0,0 +1,41 @@
+﻿[Environment]::SetEnvironmentVariable(
+    "Path",
+    "D:\Applications\Local Binaries;" + [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User),
+    [EnvironmentVariableTarget]::User)
+
+[Environment]::SetEnvironmentVariable(
+    "Path",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";D:\Applications\Racket",
+    [EnvironmentVariableTarget]::User)
+
+[Environment]::SetEnvironmentVariable(
+    "HOME",
+    "D:\git-home",
+    [EnvironmentVariableTarget]::User)
+
+[Environment]::SetEnvironmentVariable(
+    "OneDrive",
+    "D:\OneDrive",
+    [EnvironmentVariableTarget]::User)
+
+[Environment]::SetEnvironmentVariable(
+    "OneDriveConsumer",
+    "D:\OneDrive",
+    [EnvironmentVariableTarget]::User)
+
+# Save the computers Taskbar
+reg export "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Streams\Desktop" "D:\git-home\bash-settings\windows\taskbar-laptop.reg"
+
+reg export "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Streams\Desktop" "D:\git-home\bash-settings\windows\taskbar-desktop.reg"
+
+# Remove
+Remove-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Streams\Desktop -Name TaskbarWinXP
+reg import "D:\git-home\bash-settings\windows\taskbar-laptop.reg"
+taskkill /f /im explorer.exe
+start explorer.exe
+
+
+Remove-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Streams\Desktop -Name TaskbarWinXP
+reg import "D:\git-home\bash-settings\windows\taskbar-desktop.reg"
+taskkill /f /im explorer.exe
+start explorer.exe
