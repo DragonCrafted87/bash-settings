6cf08e2f7aee743f7030989ab3f41c6e0fa1f5fe
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-06-13 13:28:46 -0500

    start configuring media pc

---
+++ setup/setup-media.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+return
+
+sudo apt install xrdp
+sudo systemctl enable xrdp
+
+
+sudo apt install software-properties-common
+sudo add-apt-repository -ysP team-xbmc/ppa
+sudo apt install kodi
+
+sudo apt install kodi-visualization-goom	
+sudo apt install kodi-visualization-projectm	
+sudo apt install kodi-visualization-shadertoy	
+sudo apt install kodi-visualization-spectrum	
+sudo apt install kodi-visualization-waveform	
+sudo apt install xbmc-visualization-fishbmc
+
