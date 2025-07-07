0d26f510dcb9a8983a483029b5ca4276348d4e4f
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2025-07-07 10:41:28 -0500

    Update running-apps-pipe-menu.sh to use wmctrl -i -R for reliable window raising
    Modified the running-apps-pipe-menu.sh script to replace `wmctrl -a $id` with `wmctrl -i -R $id`. This ensures that selecting an item from the running-apps menu in Openbox consistently brings the selected window to the front and focuses it.

--- setup/openSUSE/window-manager-openbox/running-apps-pipe-menu.sh
+++ setup/openSUSE/window-manager-openbox/running-apps-pipe-menu.sh
@@ -4,7 +4,7 @@ wmctrl -l | while read -r window; do
   id=$(echo $window | cut -d' ' -f1)
   title=$(echo $window | cut -d' ' -f4-)
   echo "<item label=\"$title\">"
-  echo "  <action name=\"Execute\"><command>wmctrl -a $id</command></action>"
+  echo "  <action name=\"Execute\"><command>wmctrl -i -R $id</command></action>"
   echo "</item>"
 done
 echo '</openbox_pipe_menu>'
